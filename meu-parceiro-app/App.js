60ea0fdd7f6742b94530a685a2e427926673d90f
Breno Andrade <breno.andrade.dev@gmail.com>
2020-06-13 02:15:59 -0300

    app inicio ok

--- meu-parceiro-app/App.js
+++ meu-parceiro-app/App.js
@@ -1,36 +1,6 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { AppLoading } from 'expo';
-import { func } from './src/constants';
+import Map from './src/components/Map';
 
-import Stack from './src/navigation/Stack';
+const App = () => <Map />;
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoading: true
-    };
-  }
-
-  render() {
-    const { isLoading } = this.state;
-
-    if (isLoading) {
-      return (
-        <AppLoading
-          onFinish={() => this.setState({ isLoading: false })}
-          startAsync={func.loadAssetsAsync}
-        />
-      );
-    }
-
-    return (
-      <React.Fragment>
-        <StatusBar barStyle="dark-content" />
-        <Stack />
-      </React.Fragment>
-    );
-  }
-}
+export default App;
