a7c2d333107cf13be29ff685232e2769edff3d73
Breno Andrade <breno.andrade.dev@gmail.com>
2020-06-14 12:54:30 -0300

    input buscar estabelecimentos

--- meu-parceiro-app/src/pages/establishment/index.jsx
+++ meu-parceiro-app/src/pages/establishment/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView, ScrollView, View, TextInput, TouchableOpacity,
 } from 'react-native';
@@ -14,53 +14,62 @@ const commonStyle = {
   marginTop: 15,
 };
 
-export default ({ navigation }) => (
-  <SafeAreaView>
-    <ScrollView style={{ backgroundColor: theme.gray1, padding: 20, minHeight: '100%' }}>
-      <View style={{
-        backgroundColor: theme.white1,
-        borderColor: theme.gray2,
-        borderWidth: 1,
-        borderRadius: 5,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-      }}
-      >
-        <TextInput
-          placeholder="Buscar estabelecimento"
-          style={{
-            paddingVertical: 10,
-            paddingHorizontal: 20,
-            fontSize: theme.fontInput,
-            flex: 1,
-          }}
-        />
-        <TouchableOpacity
-          onPress={() => {}}
-          style={{
-            width: 37,
-            height: 37,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
+export default ({ navigation }) => {
+  const [term, setTerm] = useState('');
+
+  const rgx = new RegExp(`${term}`, 'ig');
+  const filtred = term ? data.filter((item) => rgx.test(item.name)) : [...data];
+
+  return (
+    <SafeAreaView>
+      <ScrollView style={{ backgroundColor: theme.gray1, padding: 20, minHeight: '100%' }}>
+        <View style={{
+          backgroundColor: theme.white1,
+          borderColor: theme.gray2,
+          borderWidth: 1,
+          borderRadius: 5,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
         >
-          <FontAwesomeIcon
-            icon={faSearch}
+          <TextInput
+            placeholder="Buscar estabelecimento"
             style={{
-              color: theme.black1,
+              paddingVertical: 10,
+              paddingHorizontal: 20,
+              fontSize: theme.fontInput,
+              flex: 1,
             }}
-            size={18}
+            onChangeText={setTerm}
           />
-        </TouchableOpacity>
-      </View>
-      { data.map((station) => (
-        <CardEstablishment
-          style={commonStyle}
-          gasStation={station}
-          onPress={() => navigation.navigate('Evaluation', { gasStation: station })}
-        />
-      )) }
-    </ScrollView>
-  </SafeAreaView>
-);
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{
+              width: 37,
+              height: 37,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faSearch}
+              style={{
+                color: theme.black1,
+              }}
+              size={18}
+            />
+          </TouchableOpacity>
+        </View>
+        { filtred.map((station) => (
+          <CardEstablishment
+            key={station.name}
+            style={commonStyle}
+            gasStation={station}
+            onPress={() => navigation.navigate('Evaluation', { gasStation: station })}
+          />
+        )) }
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
